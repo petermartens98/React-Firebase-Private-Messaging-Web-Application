bab70b19b123bd76863495ae1f261f97a333d12a
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-24 09:48:05 -0400

    Update firebase.js

--- App/src/firebase.js
+++ App/src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD40r6CK4T7JFO20XGlyMG-EIL-FRUDlg8",
-  authDomain: "peter-chat-f09bb.firebaseapp.com",
-  projectId: "peter-chat-f09bb",
-  storageBucket: "peter-chat-f09bb.appspot.com",
-  messagingSenderId: "677628828578",
-  appId: "1:677628828578:web:01ff6f2c04c306546cb0fb",
-  measurementId: "G-2MG6K0HKXT"
+  apiKey: "xxx",
+  authDomain: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx",
+  measurementId: "xxx"
 };
 
 // Initialize Firebase
